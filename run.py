a9a724d0f1915a9603b952bc811468eaf6c62eb4
nimowairimu <nimowairimu25@gmail.com>
2021-02-14 23:43:56 +0300

    update methods to show the script executability.

--- run.py
+++ run.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3.6
 from credentials import Credential
+
+def create_credential(account,username,password):
+    """
+    Function that creates new credentials 
+    """
+    new_credential = Credential(account,username,password)
+    return new_credential
+
+def save_credential(credential):
+    '''
+    Function to save credential
+    '''
+    credential.save_credential()
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
+
+def find_credential(account):
+    '''
+    Function that finds a credential by its account  and returns the credential
+    '''
+    return Contact.find_by_number(number)
+
+
